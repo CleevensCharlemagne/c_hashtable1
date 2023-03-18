9028b11832e86fc3f7a9aad0392a563b99f11767
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-17 22:07:46 -0400

    update

--- main.c
+++ main.c
@@ -47,6 +47,15 @@ void insert(int key,int data) {
    //get the hash
    int hashIndex = hashCode(key);
 
+   //move in array until an empty or deleted cell
+   while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1) {
+      //go to next cell
+      ++hashIndex;
 
+      //wrap around the table
+      hashIndex %= SIZE;
+   }
+
+   hashArray[hashIndex] = item;
 }
 
