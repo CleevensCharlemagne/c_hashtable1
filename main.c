34b758059865e41839b00860fa51b40fb2a956c5
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-17 22:06:41 -0400

    update

--- main.c
+++ main.c
@@ -22,7 +22,14 @@ struct DataItem *search(int key) {
    //get the hash
    int hashIndex = hashCode(key);
 
+   //move in array until an empty
+   while(hashArray[hashIndex] != NULL) {
 
+      if(hashArray[hashIndex]->key == key)
+         return hashArray[hashIndex];
+
+
+   }
 
    return NULL;
 }
